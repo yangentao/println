52b57e6a7044808ea118ff01378121e13948b285
Entao <entaoyang@163.com>
2025-11-25 12:05:01 +0800

    workspace

--- example/println_example.dart
+++ example/println_example.dart
@@ -1,5 +1,7 @@
 import 'package:println/println.dart';
 
 void main() {
-
+  println(1, 2, 3, name: "entao", addr: "jinan");
+  // 1 2 3, addr:jinan, name:entao
+  println(addr: "jinan", 1, 2, 3, name: "entao");
 }
